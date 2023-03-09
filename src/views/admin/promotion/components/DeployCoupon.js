6361042116bb1477dbec766f3eda187c480c78c0
Priyansh Salian <79307288+priyanshpsalian@users.noreply.github.com>
2023-03-09 18:27:02 +0530

    integrated backend to Deploy Coupon

--- src/views/admin/promotion/components/DeployCoupon.js
+++ src/views/admin/promotion/components/DeployCoupon.js
@@ -2,28 +2,47 @@ import React, { useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
 
 function DeployCoupon() {
+  const [products, setProducts] = useState([]);
+  const [company, setCompany] = useState([]);
+  useEffect(() => {
+    getProducts();
+    getCompany();
+  }, []);
+
+  const getProducts = async () => {
+    let result = await fetch(`http://localhost:3000/productType`);
+    result = await result.json();
+    setProducts(result);
+    console.log(result[0].programName);
+  };
+  const getCompany = async () => {
+    let result = await fetch(`http://localhost:3000/company`);
+    result = await result.json();
+    setCompany(result);
+  };
   const navigate = useNavigate();
   const [user, setUser] = useState({
-    companyName: "",
-    companySite: "",
-    companyUniqueId: "",
+    programId: "",
+    companyId: "",
+    noOfCoupon: "",
   });
   const handleChange = (e) => {
     const { name, value } = e.target;
+    // console.log(name,"name");
     setUser({
       ...user,
       [name]: value,
     });
 
-    console.log(user.companyName);
+    console.log(user);
   };
   const register = async () => {
     // console.log("ll");
-    const { companyName, companySite, companyUniqueId } = user;
+    const { programId, companyId, noOfCoupon } = user;
 
-    if (companyName && companySite && companyUniqueId) {
+    if (programId && companyId && noOfCoupon) {
       console.log(user);
-      let result = await fetch("http://localhost:3000/company", {
+      let result = await fetch("http://localhost:3000/combined", {
         method: "post",
         body: JSON.stringify(user),
         headers: {
@@ -33,7 +52,7 @@ function DeployCoupon() {
       });
       result = await result.json();
       //   console.log(result, "result");
-      localStorage.setItem("admin", JSON.stringify(result));
+      // localStorage.setItem("admin", JSON.stringify(result));
       // navigate("/");
       // <Redirect to="/" />;
     } else {
@@ -47,19 +66,24 @@ function DeployCoupon() {
           htmlFor="Ecoms"
           className={`mt-5 ml-3 text-xl font-bold text-navy-700 dark:text-white`}
         >
-          Select E-commerce
+          Select Coupon Type
         </label>
         <div className="relative">
           <select
+            name="programId"
+            onChange={handleChange}
             className="mt-3 mr-3 block w-full appearance-none rounded-xl border border-gray-200 bg-formBg py-3 px-4 pr-8 leading-tight text-gray-700 focus:border-gray-500 focus:bg-white focus:outline-none"
             id="grid-state"
           >
+            {products.map((p, index) => (
+              <option value={p._id}>{p.programName}</option>
+            ))}
             <option>Coupons</option>
-            <option>Loyalty Cards</option>
+            {/* <option>Loyalty Cards</option>
             <option>Promotions</option>
             <option>Discount Code</option>
             <option>Buy X get Y</option>
-            <option>Next Order Coupons</option>
+            <option>Next Order Coupons</option> */}
           </select>
           <div className="pointer-events-none absolute inset-y-0 right-0 flex items-center px-2 text-gray-700">
             <svg
@@ -77,19 +101,24 @@ function DeployCoupon() {
           htmlFor="progType"
           className={`mt-5 ml-3 text-xl font-bold text-navy-700 dark:text-white`}
         >
-          Available Coupons
+          Select Company
         </label>
         <div className="relative">
           <select
+            name="companyId"
+            onChange={handleChange}
             className="mt-3 mr-3 block w-full appearance-none rounded-xl border border-gray-200 bg-formBg py-3 px-4 pr-8 leading-tight text-gray-700 focus:border-gray-500 focus:bg-white focus:outline-none"
             id="grid-state"
           >
+            {company.map((p, index) => (
+              <option value={p._id}>{p.companyName}</option>
+            ))}
             <option>Coupons</option>
-            <option>Loyalty Cards</option>
+            {/* <option>Loyalty Cards</option>
             <option>Promotions</option>
             <option>Discount Code</option>
             <option>Buy X get Y</option>
-            <option>Next Order Coupons</option>
+            <option>Next Order Coupons</option> */}
           </select>
           <div className="pointer-events-none absolute inset-y-0 right-0 flex items-center px-2 text-gray-700">
             <svg
@@ -112,6 +141,9 @@ function DeployCoupon() {
         </label>
 
         <input
+          onChange={handleChange}
+          name="noOfCoupon"
+          value={user.noOfCoupon}
           type="text"
           id="id1"
           placeholder=""
@@ -122,6 +154,7 @@ function DeployCoupon() {
 
       <div className="mt-5 flex justify-center">
         <button
+          onClick={register}
           type="submit"
           className="text-blue mt-7 w-3/5 rounded-xl border bg-ourTheme py-3 px-6 text-xl font-bold hover:bg-ourDarkTheme"
         >
