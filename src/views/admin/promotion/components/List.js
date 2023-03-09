d76b44876e40517a5bf2c8289fc34f101053ebf6
Priyansh Salian <79307288+priyanshpsalian@users.noreply.github.com>
2023-03-10 00:46:19 +0530

    added the list page

--- src/views/admin/promotion/components/List.js
+++ src/views/admin/promotion/components/List.js
@@ -1,13 +1,74 @@
-import React from 'react'
-
-
+import React, { useEffect, useState } from "react";
+import ComplexTable from "views/admin/default/components/CouponTable";
+const columnsDataComplex = [
+  {
+    Header: "Company Name",
+    accessor: "name",
+  },
+  // {
+  //   Header: "STATUS",
+  //   accessor: "status",
+  // },
+  {
+    Header: "Company Url",
+    accessor: "date",
+  },
+  {
+    Header: "Company Id",
+    accessor: "progress",
+  },
+];
+// const tableDataComplex = [
+//   {
+//     name: "Marketplace",
+//     date: "24.Jan.2021",
+//     progress: 30,
+//   },
+//   {
+//     name: "Marketplace",
+//     date: "30.Dec.2021",
+//     progress: 30,
+//   },
+//   {
+//     name: "Marketplace",
+//     date: "20.May.2021",
+//     progress: 30,
+//   },
+//   {
+//     name: "Marketplace",
+//     date: "12.Jul.2021",
+//     progress: 30,
+//   },
+// ];
 const List = () => {
-    const data = {
+  const [products, setProducts] = useState([]);
+  useEffect(() => {
+    getProducts();
+    // makeTable();
+  }, []);
 
-    }
+  const getProducts = async () => {
+    let result = await fetch(`http://localhost:3000/company`);
+    result = await result.json() ;
+    const newData = result.map((item) => {
+      return {
+        name: item.companyName,
+        date: item.companySite,
+        progress: item.companyUniqueId,
+      };
+    });
+    setProducts(newData);
+  };
+
   return (
-    <div>List</div>
-  )
-}
+    <>
+      <div>List</div>
+      <ComplexTable
+        columnsData={columnsDataComplex}
+        tableData={products}
+      />
+    </>
+  );
+};
 
-export default List
+export default List;
