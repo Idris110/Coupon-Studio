595b0685e39e3c12eea90e7e4fdcffd85c628d45
Shivam Trivedi <strivedi947@gmail.com>
2023-03-11 11:44:59 +0530

    Coupon Generator Created

--- src/views/admin/promotion/Promotion.js
+++ src/views/admin/promotion/Promotion.js
@@ -12,21 +12,20 @@ const Promotion = () => {
   const [selectedDay, setSelectedDay] = useState(defaultValue);
   const [details, setDetails] = useState({
     promotionName: "",
-    promotionType: "",
-    onProductType: "Select Product",
+    promotionType: "New Customer",
+    onProductType: "Clothing",
     targetAudience: "",
     couponLength: "",
-    couponCodeType: "",
+    couponCodeType: "AlphaNumeric",
     couponCode: "",
     usage: {
-      type: "",
+      type: "Limited",
       limit: 1
     },
     minPurchase: 0,
     maxDiscount: 0,
-    porductType: "",
     reward: {
-      type: "",
+      type: "Fixed Amount",
       amount: 0
     },
     expiryDate: {
@@ -35,6 +34,29 @@ const Promotion = () => {
       day: 11
     }
   });
+  const generateCode = (type, length) => {
+    let alphanum = "";
+    if(type === "Numeric")
+      alphanum = "0123456789"
+    else if(type === "Alphabetic")
+      alphanum = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz"
+    else if(type === "AlphaNumeric")
+      alphanum = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz01234567890123456789";
+
+    var code = "";
+
+    for (var i = 0; i < length; i++) {
+      code += alphanum.charAt(Math.floor(Math.random() * alphanum.length));
+    }
+    console.log(code);
+    return code;
+    // if (generated.indexOf(code) === -1) {
+    //   generated.push(code);
+    // } else {
+    //   generateCode(type, length);
+    // }
+    
+  }
   // const Change = (e) => {
   //   console.log(e);
   //   console.log(details);
@@ -44,6 +66,10 @@ const Promotion = () => {
   const handleSubmit = async (e) => {
     e.preventDefault();
     // console.log(e);
+    if(details.couponCodeType !== "Custom"){
+      let code = generateCode(details.couponCodeType, details.couponLength)
+      setDetails({...details, couponCode: code});
+    }
     console.log(details);
     let result = await fetch("http://localhost:3000/productType", {
       method: "post",
@@ -159,15 +185,6 @@ const Promotion = () => {
                   Select Product Category: &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp;
                 </label>
 
-                {/* <input
-                  value={details.porductType}
-                  type="text"
-                  id="id1"
-                  onChange={(e) => { setDetails({ ...details, porductType: e.target.value }) }}
-                  placeholder="Product Type"
-                  disabled={false}
-                  className={`mt-3 h-15 rounded-xl border p-2 text-lg outline-none pl-5 bg-formBg`}
-                /> */}
                 <div className="relative mt-1">
                   <select name="couponType"
                     value={details.onProductType}
@@ -204,7 +221,7 @@ const Promotion = () => {
                         setDetails({ ...details, reward: { type: x, amount: details.reward.amount } })
                       }}
                       className="block appearance-none w-full bg-formBg border border-gray-200 text-gray-700 mr-3 py-3 px-4 pr-8 rounded-xl leading-tight focus:outline-none focus:bg-white focus:border-gray-500" id="grid-state">
-                      <option value="Absolute">Absolute</option>
+                      <option value="Fixed Amount">Absolute</option>
                       <option value="Percentage">Percentage</option>
                     </select>
                     <div className="pointer-events-none absolute inset-y-0 right-0 flex items-center px-2 text-gray-700">
@@ -239,10 +256,11 @@ const Promotion = () => {
               </label>
 
               <input
+                value={details.targetAudience}
                 onChange={(e) => { setDetails({ ...details, targetAudience: e.target.value }) }}
                 type="text"
                 id="id1"
-                placeholder="Max applicable discount"
+                placeholder="Target Audience"
                 disabled={false}
                 className={`flex items-center justify-center w-full mt-3 h-15 rounded-xl border p-2 text-lg outline-none pl-5 bg-formBg`}
               />
@@ -264,11 +282,11 @@ const Promotion = () => {
                       value={details.usage.type}
                       onChange={(e) => {
                         let x = e.target.value;
-                        e.target.value = "unlimited" ? setDetails({ ...details, usage: { type: x, limit: -1 } }) : setDetails({ ...details, usage: { type: x, limit: details.usage.limit } })
+                        e.target.value = "Unlimited" ? setDetails({ ...details, usage: { type: x, limit: -1 } }) : setDetails({ ...details, usage: { type: x, limit: details.usage.limit } })
                       }}
                       className="block appearance-none w-full bg-formBg border border-gray-200 text-gray-700 mr-3 py-3 px-4 pr-8 rounded-xl leading-tight focus:outline-none focus:bg-white focus:border-gray-500" id="grid-state">
-                      <option value="limited">Limited</option>
-                      <option value="unlimited">Unlimited</option>
+                      <option value="Limited">Limited</option>
+                      <option value="Unlimited">Unlimited</option>
                     </select>
                     <div className="pointer-events-none absolute inset-y-0 right-0 flex items-center px-2 text-gray-700">
                       <svg className="fill-current h-4 w-4" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20"><path d="M9.293 12.95l.707.707L15.657 8l-1.414-1.414L10 10.828 5.757 6.586 4.343 8z" /></svg>
@@ -307,7 +325,6 @@ const Promotion = () => {
                     value={selectedDay}
                     colorPrimary="#422afb" // added this
                     onChange={(e) => {
-                      // let x = e.target.value;
                       console.log(e);
                       setDetails({
                         ...details, expiryDate: {
@@ -359,12 +376,13 @@ const Promotion = () => {
 
                 <div className="relative mt-1">
                   <select name="couponType"
-                    value={details.promotionType}
-                    onChange={(e) => { setDetails({ ...details, promotionType: e.target.value }) }}
+                    value={details.couponCodeType}
+                    onChange={(e) => { setDetails({ ...details, couponCodeType: e.target.value }) }}
                     className="block appearance-none w-full bg-formBg border border-gray-200 text-gray-700 mt-3 mr-3 py-3 px-4 pr-8 rounded-xl leading-tight focus:outline-none focus:bg-white focus:border-gray-500" id="grid-state">
-                    <option value="Coupons">AlphaNumeric</option>
-                    <option value="LoyaltyCards">Numeric</option>
-                    <option value="Promotions">Alphabetic</option>
+                    <option value="AlphaNumeric">AlphaNumeric</option>
+                    <option value="Numeric">Numeric</option>
+                    <option value="Alphabetic">Alphabetic</option>
+                    <option value="Custom">Custom</option>
                   </select>
                   <div className="pointer-events-none absolute inset-y-0 right-0 flex items-center px-2 text-gray-700">
                     <svg className="fill-current h-4 w-4" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20"><path d="M9.293 12.95l.707.707L15.657 8l-1.414-1.414L10 10.828 5.757 6.586 4.343 8z" /></svg>
@@ -375,7 +393,7 @@ const Promotion = () => {
 
             <div className="mt-10 grid grid-cols-1 md:grid-cols-2 lg:grid-cols-2 gap-6">
               <div className="flex justify-center">
-                <button type="submit" class="text-blue w-full rounded-xl bg-ourTheme text-xl font-bold hover:bg-ourDarkTheme  hover:text-lightPrimary">
+                <button type="submit" class="text-blue h-[50px] w-full rounded-xl bg-ourTheme text-xl font-bold hover:bg-ourDarkTheme  hover:text-lightPrimary">
                   Generate Coupon
                 </button>
               </div>
@@ -385,9 +403,9 @@ const Promotion = () => {
                 id="id1"
                 value={details.couponCode}
                 onChange={(e) => { setDetails({ ...details, couponCode: e.target.value }) }}
-                placeholder="Or Enter Custom Code"
-                disabled={false}
-                className={`flex items-center justify-center w-full h-15 rounded-xl border p-2 text-lg outline-none pl-5 bg-formBg`}
+                placeholder="Enter Custom Code"
+                disabled={details.couponCodeType !== "Custom" ? true : false}
+                className={` flex items-center justify-center w-full h-15 rounded-xl border p-2 text-lg outline-none pl-5 bg-formBg`}
               />
             </div>
           </div>
