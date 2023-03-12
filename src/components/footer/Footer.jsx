182040ac44b5839649750409b99facf2285a51e6
Shivam Trivedi <strivedi947@gmail.com>
2023-03-12 10:53:38 +0530

    Final footer changes

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -11,7 +11,7 @@ const Footer = () => {
           <li>
             <a
               target="blank"
-              href="mailto:hello@simmmple.com"
+              href="mailto:strivedi947@gmail.com"
               className="text-base font-medium text-gray-600 hover:text-gray-600"
             >
               Support
@@ -20,7 +20,7 @@ const Footer = () => {
           <li>
             <a
               target="blank"
-              href="https://simmmple.com/licenses"
+              href="#"
               className="text-base font-medium text-gray-600 hover:text-gray-600"
             >
               License
@@ -29,7 +29,7 @@ const Footer = () => {
           <li>
             <a
               target="blank"
-              href="https://simmmple.com/terms-of-service"
+              href="#"
               className="text-base font-medium text-gray-600 hover:text-gray-600"
             >
               Terms of Use
@@ -38,7 +38,7 @@ const Footer = () => {
           <li>
             <a
               target="blank"
-              href="https://blog.horizon-ui.com/"
+              href="#"
               className="text-base font-medium text-gray-600 hover:text-gray-600"
             >
               Blog
